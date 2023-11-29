d409d114f77bddb362f77e598fc374bc6d9f2989
lordesi <152215798+lordesi@users.noreply.github.com>
2023-11-29 21:26:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,85 @@
-window.addEventListener("DOMContentLoaded",() => {
-	const clock = new ProgressClock("#clock");
-});
-
 class ProgressClock {
-	constructor(qs) {
-		this.el = document.querySelector(qs);
-		this.time = 0;
-		this.updateTimeout = null;
-		this.ringTimeouts = [];
-		this.update();
-	}
-	getDayOfWeek(day) {
-		switch (day) {
-			case 1:
-				return "Monday";
-			case 2:
-				return "Tuesday";
-			case 3:
-				return "Wednesday";
-			case 4:
-				return "Thursday";
-			case 5:
-				return "Friday";
-			case 6:
-				return "Saturday";
-			default:
-				return "Sunday";
-		}
-	}
-	getMonthInfo(mo,yr) {
-		switch (mo) {
-			case 1:
-				return { name: "February", days: yr % 4 === 0 ? 29 : 28 };
-			case 2:
-				return { name: "March", days: 31 };
-			case 3:
-				return { name: "April", days: 30 };
-			case 4:
-				return { name: "May", days: 31 };
-			case 5:
-				return { name: "June", days: 30 };
-			case 6:
-				return { name: "July", days: 31 };
-			case 7:
-				return { name: "August", days: 31 };
-			case 8:
-				return { name: "September", days: 30 };
-			case 9:
-				return { name: "October", days: 31 };
-			case 10:
-				return { name: "November", days: 30 };
-			case 11:
-				return { name: "December", days: 31 };
-			default:
-				return { name: "January", days: 31 };
-		}
-	}
-	update() {
-		this.time = new Date();
+    constructor(qs) {
+        this.el = document.querySelector(qs);
+        this.time = 0;
+        this.updateTimeout = null;
+        this.ringTimeouts = [];
+        this.update();
+    }
 
-		if (this.el) {
-			// date and time
-			const dayOfWeek = this.time.getDay();
-			const year = this.time.getFullYear();
-			const month = this.time.getMonth();
-			const day = this.time.getDate();
-			const hr = this.time.getHours();
-			const min = this.time.getMinutes();
-			const sec = this.time.getSeconds();
-			const dayOfWeekName = this.getDayOfWeek(dayOfWeek);
-			const monthInfo = this.getMonthInfo(month,year);
-			const m_progress = sec / 60;
-			const h_progress = (min + m_progress) / 60;
-			const d_progress = (hr + h_progress) / 24;
-			const mo_progress = ((day - 1) + d_progress) / monthInfo.days;
-			const units = [
-				{
-					label: "w",
-					value: dayOfWeekName
-				},
-				{
-					label: "mo",
-					value: monthInfo.name,
-					progress: mo_progress
-				},
-				{
-					label: "d", 
-					value: day,
-					progress: d_progress
-				},
-				{
-					label: "h", 
-					value: hr > 12 ? hr - 12 : hr,
-					progress: h_progress
-				},
-				{
+    getDayOfWeek(day) {
+        // Restituisce il giorno della settimana
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return days[day];
+    }
+
+    getMonthInfo(month, year) {
+        // Restituisce informazioni sul mese
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const monthLengths = [31, (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        return { name: monthNames[month], days: monthLengths[month] };
+    }
+
+    update() {
+        this.time = new Date();
+        const dayOfWeek = this.time.getDay();
+        const year = this.time.getFullYear();
+        const month = this.time.getMonth();
+        const day = this.time.getDate();
+        const hour = this.time.getHours();
+        const minute = this.time.getMinutes();
+        const second = this.time.getSeconds();
+
+        // Ottenimento del giorno della settimana e informazioni sul mese
+        const dayOfWeekName = this.getDayOfWeek(dayOfWeek);
+        const monthInfo = this.getMonthInfo(month, year);
+
+        // Calcolo dei progressi per i diversi livelli dell'orologio
+        const secondsProgress = second / 60;
+        const minutesProgress = (minute + secondsProgress) / 60;
+        const hoursProgress = (hour + minutesProgress) / 12;
+        const daysProgress = (hour + minutesProgress) / 24;
+        const monthsProgress = ((day - 1) + daysProgress) / monthInfo.days;
+
+        // Aggiornamento visuale dell'orologio
+        this.updateClockDisplay([
+            { label: "w", value: dayOfWeekName },
+            { label: "mo", value: monthInfo.name, progress: monthsProgress },
+            { label: "d", value: day, progress: daysProgress },
+            { label: "h", value: hour, progress: hoursProgress },
+            { label: "m", value: minute, progress: minutesProgress },
+            { label: "s", value: second, progress: secondsProgress },
+        ]);
+
+        this.scheduleNextUpdate();
+    }
+
+    updateClockDisplay(units) {
+        units.forEach(unit => {
+            const element = this.el.querySelector(`[data-unit="${unit.label}"]`);
+            if (element) {
+                element.textContent = unit.value < 10 ? `0${unit.value}` : unit.value;
+            }
+
+            const ring = this.el.querySelector(`[data-ring="${unit.label}"]`);
+            if (ring) {
+                const circumference = 2 * Math.PI * ring.getAttribute("r");
+                const strokeDashArray = circumference.toFixed(2);
+                const strokeDashOffset = (1 - unit.progress) * circumference;
+                ring.style.strokeDasharray = `${strokeDashArray} ${strokeDashArray}`;
+                ring.style.strokeDashoffset = `${strokeDashOffset.toFixed(2)}px`;
+            }
+        });
+    }
+
+    scheduleNextUpdate() {
+        this.updateTimeout = setTimeout(() => this.update(), 1000);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const clock = new ProgressClock("#clock");
+});
 					label: "m", 
 					value: min < 10 ? "0" + min : min,
 					progress: m_progress
